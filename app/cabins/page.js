238ad0a229953c235cd528042757648832b15b92
stella <parkkr@valofe.com>
2024-10-24 16:43:44 +0900

    [U] cabins 에 api 연결

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
-function Page () {
+export default async function Page () {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
  return (
   <>
-    cabins
+    <h1>cabins</h1>
+    {data.length>0 &&
+      (<ul>
+        {data?.map((user) =>
+          <li key={user.id}>{user.name}</li>
+        )}
+      </ul>)
+    }
   </>
- );}
-
-export default Page;
+ );
+}
