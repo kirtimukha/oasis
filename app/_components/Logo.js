b756217fed7b9626ffbcc11864147a944bc7d8d4
stella <parkkr@valofe.com>
2024-11-01 18:23:56 +0900

    [U] 이미지 컴포넌트 사용하기

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,9 +1,15 @@
 import Link from "next/link";
+import Image from "next/image";
 
 function Logo() {
     return (
       <Link href="/" className="flex items-center gap-4 z-10">
-          <img src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
+          <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
+          {/* Image 컴포넌트의 장점 */}
+          {/* 1. 정확한 w && h 사이즈 필요로 함 - 레이아웃 변경을 방지한다. */}
+          {/* 2. 이미지를 자동 변환한다. webP등으로 */}
+          {/* 3. 자동으로 레이지 로드한다. - 뷰포트에 들어올 때만 로드 함 */}
+        
           <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
