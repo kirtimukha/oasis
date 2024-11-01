df5c4fa15e2e07d7ed9a82d47609884ec0928917
stella <parkkr@valofe.com>
2024-11-01 15:42:37 +0900

    [U] cabin - 불필요한 json 삭제

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,18 +1,9 @@
 import React from 'react';
 
 export default async function Page () {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
  return (
   <>
     <h1>cabins</h1>
-    {data.length>0 &&
-      (<ul>
-        {data?.map((user) =>
-          <li key={user.id}>{user.name}</li>
-        )}
-      </ul>)
-    }
   </>
  );
 }
