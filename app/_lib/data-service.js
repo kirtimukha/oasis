af73665ed5a0ef193cf3188a7086fc967cd6b605
stella <parkkr@valofe.com>
2024-11-14 17:53:24 +0900

    [U] data-service 에서 속도 체크용 셋타임아웃 삭제

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -43,7 +43,7 @@ export const getCabins = async function () {
 		.select('id, name, maxCapacity, regularPrice, discount, image')
 		.order('name');
 	// For testing
-	await new Promise(res => setTimeout(res, 1000));
+	//await new Promise(res => setTimeout(res, 2000));
 	if (error) {
 		console.error(error);
 		throw new Error('Cabins could not be loaded');
