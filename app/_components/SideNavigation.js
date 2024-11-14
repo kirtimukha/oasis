19db63e610c85539904ade6a5eba4a60e63656bc
stella <parkkr@valofe.com>
2024-11-14 17:55:35 +0900

    [U] "use client" 적용

--- app/_components/SideNavigation.js
+++ app/_components/SideNavigation.js
@@ -1,9 +1,13 @@
+'use client';
+
 import {
 	CalendarDaysIcon,
 	HomeIcon,
 	UserIcon,
 } from '@heroicons/react/24/solid';
 import SignOutButton from '@/app/_components/SignOutButton';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 const navLinks = [
 	{
@@ -24,20 +28,20 @@ const navLinks = [
 ];
 
 function SideNavigation() {
+	const pathname = usePathname();
+
 	return (
 		<nav className='border-r border-primary-900'>
 			<ul className='flex flex-col gap-2 h-full text-lg'>
 				{ navLinks.map(link => (
 					<li key={ link.name }>
-						<a
-							className={
-								'py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200'
-							}
+						<Link
+							className={ `py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 ${pathname === link.href ? 'bg-primary-800' : ''}` }
 							href={ link.href }
 						>
 							{ link.icon }
 							<span>{ link.name }</span>
-						</a>
+						</Link>
 					</li>
 				)) }
 
