de26e4c3c7d7b2a3097c7ae1c0d5087883344894
stella <parkkr@valofe.com>
2024-11-14 15:07:09 +0900

    [U] 하나의 컴포넌트가 - 서버 컴포넌트, 컴포넌트 인스턴스로 사용되는 예시

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -3,25 +3,25 @@ import Image from 'next/image';
 import logo from '@/public/logo.png';
 
 function Logo() {
-  return (
-    <Link href='/' className='flex items-center gap-4 z-10'>
-      { /*<Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />*/ }
-      { /* Image 컴포넌트의 장점 */ }
-      { /* 1. 정확한 w && h 사이즈 필요로 함 - 레이아웃 변경을 방지한다. */ }
-      { /* 2. 이미지를 자동 변환한다. webP등으로 */ }{ ' ' }
-      { /* 3. 자동으로 레이지 로드한다. - 뷰포트에 들어올 때만 로드 함 */ }
-      <Image
-        src={ logo }
-        alt='The Wild Oasis logo'
-        width='60'
-        height='60'
-        quality={ 100 }
-      />
-      <span className='text-xl font-semibold text-primary-100'>
-        The Wild Oasis
-      </span>
-    </Link>
-  );
+	return (
+		<Link href='/' className='flex items-center gap-4 z-10'>
+			{ /*<Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />*/ }
+			{ /* Image 컴포넌트의 장점 */ }
+			{ /* 1. 정확한 w && h 사이즈 필요로 함 - 레이아웃 변경을 방지한다. */ }
+			{ /* 2. 이미지를 자동 변환한다. webP등으로 */ }{ ' ' }
+			{ /* 3. 자동으로 레이지 로드한다. - 뷰포트에 들어올 때만 로드 함 */ }
+			<Image
+				src={ logo }
+				alt='The Wild Oasis logo'
+				width='60'
+				height='60'
+				quality={ 100 }
+			/>
+			<span className='text-xl font-semibold text-primary-100'>
+				The Wild Oasis
+			</span>
+		</Link>
+	);
 }
 
 export default Logo;
